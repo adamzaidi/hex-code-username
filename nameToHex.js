d06496fdf48499bf12f5321b5f5e0677affe225f
Adam Zaidi <93174158+adamzaidi@users.noreply.github.com>
2026-03-08 10:42:11 -0400

    Initial commit

---
+++ nameToHex.js
@@ -0,0 +1,13 @@
+function nameToHex(name) {
+  let hash = 0;
+  for (let i = 0; i < name.length; i++) {
+    hash = name.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  const color = (hash & 0x00ffffff).toString(16).toUpperCase();
+  return "#" + color.padStart(6, "0");
+}
+
+// Examples
+console.log(nameToHex("John Smith"));
+console.log(nameToHex("Jane Doe"));
+console.log(nameToHex("Syed Adam Abbas Zaidi"));
